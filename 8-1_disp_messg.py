9ec6877fb69d6401f0039bd6e89a6c7053861438
Robert Neville <robertneville777@gmail.com>
2018-08-09 00:44:32 -0400

    Added the first two scripts for Ch8

---
+++ 8-1_disp_messg.py
@@ -0,0 +1,5 @@
+def display_message():
+    """ Display a simple greeting."""
+    print("Hey everyone, learning how to write functions.")
+
+display_message()
---
+++ 8-2_fav_book.py
@@ -0,0 +1,5 @@
+def favorite_book(title):
+    """Display message about title parametere"""
+    print("One of my favorite books is " + title.title() + ".")
+
+favorite_book("i am legend")
