6174823b507cd43e51885a9b9c7bb2ae6daef693
Robert Neville <robertneville777@gmail.com>
2018-09-07 01:32:36 -0400

    Added docstrings to dice class

--- dice.py
+++ dice.py
@@ -1,13 +1,19 @@
+""" A class to model rolling die """
+
 from random import randint
 
 class Die():
+    """ Simple model for rolling die """
 
     def __init__(self):
+        """ Initialize attributes for die """
         self.sides = 6
 
     def roll_die(self):
+        """ Roll die to produce random number between 1 and num_sides"""
         roll_result = randint(1,self.sides)
         print('You rolled a ' + str(roll_result) + '.')
 
     def set_num_sides(self,num_sides):
+        """ Set upper-limit of random number, aka number of sides """
         self.sides = num_sides
